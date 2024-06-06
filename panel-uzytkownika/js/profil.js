8ea786a7e5a0c8ff7759b556b5802bb243a2eb26
Playdovsky <144317216+Playdovsky@users.noreply.github.com>
2024-06-06 15:51:49 +0200

    Profile modification validation

--- panel-uzytkownika/js/profil.js
+++ panel-uzytkownika/js/profil.js
@@ -1,25 +1,3 @@
-document.getElementById("profileForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var formData = new FormData(this);
-
-    $.ajax({
-        url: $(this).attr('action'),
-        type: 'POST',
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function(response) {
-            console.log(response);
-            alert('Zmiany zostały zapisane.');
-            window.location.replace("panel-uzytkownika.php");
-        },
-        error: function(xhr, status, error) {
-            console.error('Wystąpił błąd podczas zapisywania zmian.', xhr.responseText);
-            alert('Wystąpił błąd podczas zapisywania zmian. Spróbuj ponownie.');
-        }
-    });
-});
-
 $(document).on('click', '#deactivateAccountBtn', function() {
     var confirmed = confirm("Czy na pewno chcesz dezaktywować konto? Utracisz dostęp do swojego konta i urządzeń z nim powiązanych, ten proces jest odwracalny tylko po skontaktowaniu się z administratorem strony.");
     if (confirmed) {
